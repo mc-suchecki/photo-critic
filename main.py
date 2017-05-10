e42d5438a96d3544768dbe8a8b2f1ebfb9d723c8
Maciej Suchecki <mc.suchecki@gmail.com>
2017-05-11 01:34:14 +0200

    Added logging compatible with heroku for debugging

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, redirect
+import logging
+from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 app.config.from_object('config')
 
 
-@app.route('/', methods=['GET'])
-def main():
+@app.route('/')
+def homepage():
   return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
