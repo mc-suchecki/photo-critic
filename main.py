0d17d8ec0e359e8d9491cbd123182637c3e5c967
Maciej Suchecki <mc.suchecki@gmail.com>
2017-05-12 01:54:02 +0200

    Fixed the transfer of image to backend; now it works properly Added JS code that shrinks the image to save bandwidth

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import logging
 import random
 import sys
+import time
+import io
+
+from PIL import Image
 from flask import Flask
 from flask import render_template
 from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
+app.logger.setLevel(logging.INFO)
 app.config.from_object('config')
 
 
@@ -18,10 +23,15 @@ def homepage():
 
 @app.route('/rate-photo', methods=["PUT"])
 def rate_photo():
-  # print(request.json)
-  # TODO so far the score is hardcoded in the backend
+  logger = logging.getLogger(__name__)
+  logger.info('Received image: {} bytes, {}.'.format(request.content_length, request.content_type))
+  logger.info('Reading data...')
+  stream = io.BytesIO(request.data)
+  img = Image.open(stream)
+  logger.info('Done. Image size is {}x{} pixels.'.format(img.size[0], img.size[1]))
+  # TODO send the photo to caffe deploy and retrieve the score
+  time.sleep(2)
   score = random.randint(1, 100)
-  print("works")
   return jsonify(score=score)
 
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,48 +1,69 @@
-document.getElementById("photo-input").addEventListener("change", readURL, true);
+document.getElementById("photo-input").addEventListener("change", onPhotoSelect, true);
 
-function readURL() {
+function onPhotoSelect() {
     var file = document.getElementById("photo-input").files[0];
     var reader = new FileReader();
-    reader.onloadend = function () {
-        // display the photo
+    reader.onloadend = displayPhoto;
+    if (file) {
+        reader.readAsDataURL(file);
+        assessPhoto(file);
+    }
+
+    function displayPhoto() {
+        // display the uploaded photo
         var photoElement = document.getElementById("photo");
         photoElement.src = reader.result;
         photoElement.classList.remove("hidden");
         // hide the drag and drop area and the aesthetics score
         document.getElementsByClassName("aspect-ratio-fix")[0].classList.add("hidden");
         document.getElementById("score").classList.add("hidden");
-        // display the spinner
-        document.getElementById("spinner").classList.remove("hidden");
-        // do the AJAX call to retrieve the score
-        var request = new XMLHttpRequest();
-        request.open("PUT", "/rate-photo");
-        request.setRequestHeader("Content-Type", "application/json");
-        request.onload = function () {
-            if (request.status === 200) {
-                var result = JSON.parse(request.responseText);
-                console.log(result.score);
-                document.getElementById("spinner").classList.add("hidden");
-                document.getElementById("score").classList.remove("hidden");
-                document.getElementById("score-bar").style["width"] = result.score + "%";
-                document.getElementById("score-text").innerText = result.score + "%";
-                if (result.score >= 75) {
-                    document.getElementById("score-bar").classList.add("progress-bar-success");
-                    document.getElementById("score-text").classList.add("text-success");
-                } else if (result.score >= 25) {
-                    document.getElementById("score-bar").classList.add("progress-bar-warning");
-                    document.getElementById("score-text").classList.add("text-warning");
-                } else {
-                    document.getElementById("score-bar").classList.add("progress-bar-danger");
-                    document.getElementById("score-text").classList.add("text-danger");
-                }
-            } else {
-                console.log("Error!");
-            }
-        };
-        request.send(JSON.stringify({photo: reader.result}));
-    };
-    if (file) {
-        reader.readAsDataURL(file);
-    } else {
     }
+}
+
+function assessPhoto(photo) {
+    // display the spinner and 'resizing...' text
+    document.getElementById("spinner").classList.remove("hidden");
+    document.getElementById("resizing").classList.remove("hidden");
+    // resize the image and send it to the backend to analyze it afterwards
+    ImageTools.resize(photo, {width: 240, height: 999}, sendPhotoToBackend);
+}
+
+function sendPhotoToBackend(blob, didItResize) {
+    // change 'resizing..' text to 'analyzing...'
+    document.getElementById("resizing").classList.add("hidden");
+    document.getElementById("analyzing").classList.remove("hidden");
+    // do the AJAX call to retrieve the score
+    var request = new XMLHttpRequest();
+    request.open("PUT", "/rate-photo");
+    // request.setRequestHeader("Content-Type", "application/json");
+    request.onload = function () {
+        if (request.status === 200) {
+            var result = JSON.parse(request.responseText);
+            displayScore(result.score);
+        }
+    };
+    console.log(blob);
+    request.send(blob);
+}
+
+function displayScore(score) {
+    // hide the spinner and show the score
+    document.getElementById("spinner").classList.add("hidden");
+    document.getElementById("score").classList.remove("hidden");
+    document.getElementById("score-text").innerText = score + "%";
+    // adjust the color of the bar and text
+    if (score >= 75) {
+        document.getElementById("score-bar").classList.add("progress-bar-success");
+        document.getElementById("score-text").classList.add("text-success");
+    } else if (score >= 25) {
+        document.getElementById("score-bar").classList.add("progress-bar-warning");
+        document.getElementById("score-text").classList.add("text-warning");
+    } else {
+        document.getElementById("score-bar").classList.add("progress-bar-danger");
+        document.getElementById("score-text").classList.add("text-danger");
+    }
+    // adjust the width of the bar
+    window.setTimeout(function () {
+        document.getElementById("score-bar").style["width"] = score + "%";
+    }, 100);
 }
