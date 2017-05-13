153c0b9e80ab5fd89d25384d8c4c5ec0cc5617e5
Maciej Suchecki <mc.suchecki@gmail.com>
2017-05-13 06:28:09 +0200

    Modified the backend to send the photo to remote server with Caffe and get the score of the photo

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import logging
-import random
 import sys
-import time
 import io
+import zmq
 
 from PIL import Image
 from flask import Flask
 from flask import render_template
 from flask import jsonify
 from flask import request
+from os import environ
+from zmq import ssh
 
+# initialize Flask web server
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.INFO)
 app.config.from_object('config')
+
+# initialize zmq message queue
+context = zmq.Context()
+port = "6666"
+ssh_password = environ.get("SSH_PASSWORD")
+app.logger.info("Connecting to titan.elka.pw.edu.pl...")
+socket = context.socket(zmq.REQ)
+ssh.tunnel_connection(socket, "tcp://localhost:6666", "msucheck@mion.elka.pw.edu.pl", password=ssh_password)
+app.logger.info("Connected!")
 
 
 @app.route('/')
@@ -23,16 +34,17 @@ def homepage():
 
 @app.route('/rate-photo', methods=["PUT"])
 def rate_photo():
-  logger = logging.getLogger(__name__)
-  logger.info('Received image: {} bytes, {}.'.format(request.content_length, request.content_type))
-  logger.info('Reading data...')
+  app.logger.info('Received image: {} bytes, {}.'.format(request.content_length, request.content_type))
+  app.logger.info('Reading data...')
   stream = io.BytesIO(request.data)
-  img = Image.open(stream)
-  logger.info('Done. Image size is {}x{} pixels.'.format(img.size[0], img.size[1]))
-  # TODO send the photo to caffe deploy and retrieve the score
-  time.sleep(2)
-  score = random.randint(1, 100)
-  return jsonify(score=score)
+  image = Image.open(stream)
+  app.logger.info('Done. Size of the image is {}x{} pixels.'.format(image.size[0], image.size[1]))
+  app.logger.info('Sending the image to titan.elka.pw.edu.pl...')
+  socket.send(request.data)
+  response = socket.recv_json()
+  app.logger.info('Done. Photo got score of {}%.'.format(response['score']))
+  return jsonify(response)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+  app.run(debug=True, use_reloader=True)
