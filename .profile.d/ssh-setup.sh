1ed0cb38e5ade9170a4aa3f829f87d0b33d34232
Maciej Suchecki <mc.suchecki@gmail.com>
2017-05-13 06:57:35 +0200

    Preloading known_hosts file to avoid the prompt

--- .profile.d/ssh-setup.sh
+++ .profile.d/ssh-setup.sh
@@ -14,6 +14,8 @@ echo "${HEROKU_PRIVATE_KEY}" > ${HOME}/.ssh/heroku_id_rsa
 chmod 600 ${HOME}/.ssh/heroku_id_rsa
 
 # Preload the known_hosts file  (see "version 2" below)
+echo 'mion.elka.pw.edu.pl,194.29.160.35 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAvxNx6lUVVGL8K7xSY/lZG1ze8FMJC1HXBPVufH4IMT9YJ2mnReCGLqjmrBQdEHfA4Avv4XjCmUkA/k7XE7Mm0TfMaISgqmkI3YMBMWUtHnNquiRlNRsuYy+IzSgOQ2JnVyqygI10LVY01469CW/3yrq8ULq05Cn02PUkYzrxTwdkXsTz60YFDxH6wq2DzXimbUlCHqKNk2Pzmbx3gufUzKGbozJKEGkOJ8ESfB1o2fuezxhobwW18s8lybz9FeT2mFbbFo31Yu5ZzRymspe4OKB1c6p/tSSquRTx3mrPNkhVs0PY7t1L4KXlAa8aVGWqrI/dZRrolpZCeCv34dsNeQ==
+' > ${HOME}/.ssh/known_hosts
 
 # Start the SSH tunnel if not already running
 SSH_CMD="ssh -f -i ${HOME}/.ssh/heroku_id_rsa -N -L 6666:localhost:3306 msucheck@mion.elka.pw.edu.pl"
